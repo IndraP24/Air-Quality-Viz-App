2e8cd739d1b209d318ddee764c2f390ddd87d3a1
IndraP24 <indrashis985@gmail.com>
2021-08-13 22:06:10 +0530

    Update usage of India geodata for map

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,18 @@
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
-import am4geodata_IndiaHigh from "@amcharts/amcharts4-geodata/worldIndiaHigh";
-import am4geodata_franceLow from "@amcharts/amcharts4-geodata/franceLow";
-import am4geodata_franceHigh from "@amcharts/amcharts4-geodata/franceHigh";
+import am4geodata_indiaHigh from "@amcharts/amcharts4-geodata/indiaHigh";
 
-// Low-detail map
+// High detail map
 var chart = am4core.create("chartdiv", am4maps.MapChart);
-chart.geodata = am4geodata_franceLow;
+chart.geodata = am4geodata_indiaHigh;
 chart.projection = new am4maps.projections.Miller();
+chart.align = "center";
+
 var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 polygonSeries.useGeodata = true;
 polygonSeries.mapPolygons.template.events.on("hit", function (ev) {
     chart.zoomToMapObject(ev.target);
 });
-var label = chart.chartContainer.createChild(am4core.Label);
-label.text = "franceLow";
 
-// High detail map
-var chart2 = am4core.create("chartdiv2", am4maps.MapChart);
-chart2.geodata = am4geodata_franceHigh;
-chart2.projection = new am4maps.projections.Miller();
-var polygonSeries = chart2.series.push(new am4maps.MapPolygonSeries());
-polygonSeries.useGeodata = true;
-polygonSeries.mapPolygons.template.events.on("hit", function (ev) {
-    chart2.zoomToMapObject(ev.target);
-});
-var label2 = chart2.chartContainer.createChild(am4core.Label);
-label2.text = "franceHigh";
+var label = chart.chartContainer.createChild(am4core.Label);
+label.text = "indiaHigh";
