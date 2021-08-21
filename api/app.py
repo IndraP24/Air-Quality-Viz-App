227b0b663c93ed83579408426ef80495c933315c
IndraP24 <indrashis985@gmail.com>
2021-08-21 23:34:02 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from lib.main import *
 
 app = FastAPI()
+
+
+class Input(BaseModel):
+    data_path: str
+    periods: int
+
 
 @app.get("/ping")
 def pong():
     return {"ping": "pong"}
 
+
+@app.post("/predict")
+def forecast(input: Input):
+    data_path = input.data_path
+    df = preprocess(data_path)
