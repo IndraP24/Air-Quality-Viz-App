1210879e9beebb7c883cd14b909a6ac6d29bb7d8
IndraP24 <indrashis985@gmail.com>
2021-08-24 01:10:14 +0530

    Add post endpoint for forecast operation

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 from lib.main import *
@@ -9,6 +9,7 @@ app = FastAPI()
 class Input(BaseModel):
     data_path: str
     periods: int
+    model: str
 
 
 @app.get("/ping")
@@ -20,3 +21,18 @@ def pong():
 def forecast(input: Input):
     data_path = input.data_path
     df = preprocess(data_path)
+
+    if input.model == "Prophet":
+        prediction_list = predict_prophet(df, input.periods)
+    elif input.model == "Arima":
+        prediction_list = predict_arima(df, input.periods)
+    else:
+        raise HTTPException(
+            status_code=400, detail="No model provided as input!")
+
+    if not prediction_list:
+        raise HTTPException(status_code=400, detail="Error in model!")
+
+    response_object = {"data_path": input.data_path, "periods": input.periods,
+                       "model": input.model, "forecasts": prediction_list}
+    return response_object
